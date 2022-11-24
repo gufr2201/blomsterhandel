0866fafd19bd2f765c6b0e23544bc03e38886957
gufr2201 <gfranchell@gmail.com>
2022-11-24 18:43:59 +0100

    add all main.js and show and hide js to produkt.js

---
+++ script/main.js
@@ -0,0 +1,70 @@
+$(document).ready(function(){
+    
+
+    $(".card-btn-info1").click(function() {
+        const displayState = $("id").css("display")
+        console.log("DISPLAY", displayState)
+        if(displayState === "block") {
+            $(this).text("Mer information")
+        }
+        else {
+            $(this).text("Dölj information")
+        }
+        $("#plant-info1").toggle()
+    });
+    $(".card-btn-info2").click(function() {
+        const displayState = $("id").css("display")
+        console.log("DISPLAY", displayState)
+        if(displayState === "block") {
+            $(this).text("Mer information")
+        }
+        else {
+            $(this).text("Dölj information")
+        }
+        $("#plant-info2").toggle()
+    });
+    $(".card-btn-info3").click(function() {
+        const displayState = $("id").css("display")
+        console.log("DISPLAY", displayState)
+        if(displayState === "block") {
+            $(this).text("Mer information")
+        }
+        else {
+            $(this).text("Dölj information")
+        }
+        $("#plant-info3").toggle()
+    });
+    $(".card-btn-info4").click(function() {
+        const displayState = $("id").css("display")
+        console.log("DISPLAY", displayState)
+        if(displayState === "block") {
+            $(this).text("Mer information")
+        }
+        else {
+            $(this).text("Dölj information")
+        }
+        $("#plant-info4").toggle()
+            });
+            $(".card-btn-info5").click(function() {
+                const displayState = $("id").css("display")
+                console.log("DISPLAY", displayState)
+                if(displayState === "block") {
+                    $(this).text("Mer information")
+                }
+                else {
+                    $(this).text("Dölj information")
+                }
+                $("#plant-info5").toggle();
+                    });
+                    $(".card-btn-info6").click(function() {
+                        const displayState = $("id").css("display")
+                        console.log("DISPLAY", displayState)
+                        if(displayState === "block") {
+                            $(this).text("Mer information")
+                        }
+                        else {
+                            $(this).text("Dölj information")
+                        }
+                        $("#plant-info6").toggle()
+                            });
+    })
